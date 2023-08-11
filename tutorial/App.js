3371ee35e93d9dae0fd731fa176328b6be0fe09a
onur akyıldız <onurakyildi41@gmail.com>
2023-08-11 21:10:59 +0300

    expo tutorial

--- tutorial/App.js
+++ tutorial/App.js
@@ -1,12 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { Header } from 'react-native/Libraries/NewAppScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <header/>
-      <home />
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
