2260992e4175b8699d4e358c868cafe03613d0a4
onur akyıldız <onurakyildi41@gmail.com>
2024-09-24 00:28:23 +0300

    demo

---
+++ insta_clone/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+function App() {
+  return (
+    <View style={styles.container}>
+      <Text>asd</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#111111',
+  },
+});
+
+export default App;
